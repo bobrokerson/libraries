feb2c0a7b48bc8102bda5288a27276d724747ac5
Dmitry Lakhov <73223982+bobrokerson@users.noreply.github.com>
2021-12-29 13:38:24 +0100

    Update Pesel.py

--- Pesel.py
+++ Pesel.py
@@ -1,3 +1,4 @@
+# option 1
 import pandas as pd
 from faker import Faker 
 
@@ -17,3 +18,22 @@ df = pd.Series(generate_ssns(10))
 dff=pd.DataFrame(df, columns=['Pesel'])
 
 display(dff)
+
+-----------------------------------------------
+#Option 2
+
+from faker import Faker
+import pandas as pd
+
+fake = Faker()
+fake = Faker('pl_PL')
+
+def generate_ssns(x):
+    output = [{"name":fake.pesel(),
+                   "year":fake.year(),
+                   "email":fake.email()} for x in range(x)]
+    return output
+
+
+faker = pd.DataFrame(generate_ssns(20))
+display(faker)
